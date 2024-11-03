c08c0b52a10504f64cb76cb649f721070e9e5064
PrathamX595 <pratham.seth.cer23@itbhu.ac.in>
2024-11-03 19:37:51 +0530

    astro islands with preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-    devToolbar: {
-    enabled: false,
-  },
-  site: "https://basicblog595.netlify.app/"
+  devToolbar: {enabled: false},
+  site: "https://basicblog595.netlify.app/",
+  integrations: [preact()]
 });
